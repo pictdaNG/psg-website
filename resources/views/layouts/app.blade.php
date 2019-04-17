c0e46d70af2c246d4f46fc385bebcbc3d5dd3702
Nanichang <nanipaul68@gmail.com>
2019-04-17 11:06:37 +0100

    added fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,73 +8,7 @@
     <title>@yield('title')</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::asset('assets/css/app.css') }}">
-    <style type="text/css">
-        #primary_nav_wrap {
-            margin-top:15px
-        }
-
-        #primary_nav_wrap ul {
-            list-style:none;
-            position:relative;
-            float:left;
-            margin:0;
-            padding:0
-        }
-
-        #primary_nav_wrap ul a {
-            display:block;
-            color:#333;
-            text-decoration:none;
-            font-weight:700;
-            font-size:12px;
-            line-height:32px;
-            padding:0 15px;
-            /*font-family:"HelveticaNeue","Helvetica Neue",Helvetica,Arial,sans-serif*/
-        }
-
-        #primary_nav_wrap ul li {
-            position:relative;
-            float:left;
-            margin:0;
-            padding:0
-        }
-
-        #primary_nav_wrap ul li.current-menu-item {
-            background:#ddd
-        }
-
-        #primary_nav_wrap ul li:hover {
-            background:#f6f6f6
-        }
-
-        #primary_nav_wrap ul ul {
-            display:none;
-            position:absolute;
-            top:100%;
-            left:0;
-            background:#fff;
-            padding:0
-        }
-
-        #primary_nav_wrap ul ul li {
-            float:none;
-            width:200px
-        }
-
-        #primary_nav_wrap ul ul a {
-            line-height:120%;
-            padding:10px 15px
-        }
-
-        #primary_nav_wrap ul ul ul {
-            top:0;
-            left:100%
-        }
-
-        #primary_nav_wrap ul li:hover > ul {
-            display:block
-        }
-    </style>
+    
     @yield('styles')
   </head>
 <body>
