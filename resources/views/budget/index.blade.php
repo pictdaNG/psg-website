26f1072cdca4c3f9bd824fe7dbda65b301ff2d80
Nanichang <nanipaul68@gmail.com>
2019-05-04 10:57:18 +0100

    added more files and audio

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -15,7 +15,33 @@
 @section('content')
     <div class="content uk-width-1-2@m">
         <h4>2019 Budget</h4>
-        <ul>
+        
+        <ul class="uk-list uk-list-striped">
+            <li>
+                <a href="{{ URL::asset('assets/documents/2019/2019-PLATEAU-STATE-APPROVED-BUDGET-1.pdf') }}">2019 PLATEAU STATE APPROVED BUDGET</a>
+            </li>
+            <li>
+                <a href="{{ URL::asset('assets/documents/2019/CITIZENS-PARTICIPATION-IN-BUDGET-PROCESS-2019-PLATEAU-CENTRAL.pdf') }}">CITIZENS PARTICIPATION IN BUDGET PROCESS 2019 PLATEAU CENTRAL</a>
+            </li>
+            <li>
+                <a href="{{ URL::asset('assets/documents/2019/CITIZENS-PARTICIPATION-IN-BUDGET-PROCESS 2019-PLATEAU-SOUTH.pdf') }}">CITIZENS PARTICIPATION IN BUDGET PROCESS 2019 PLATEAU SOUTH</a>
+            </li>
+             <li>
+                <a href="{{ URL::asset('assets/documents/2019/CITIZENS- PARTICIPATION IN-BUDGET-PROCESS-2019-PLATEAU-NORTH.pdf') }}">CITIZENS PARTICIPATION IN BUDGET PROCESS 2019 PLATEAU NORTH</a>
+            </li>
+            <li>
+                <audio controls>
+                  <source src="{{ URL::asset('assets/documents/2019/Advertisement-for-Citizens-Budget.mpeg') }}" type="audio/mpeg">
+                  <p>Your browser doesn't support audio. Here is
+                     a <a href="{{ URL::asset('assets/documents/2019/Advertisement-for-Citizens-Budget.mpeg') }}" download>link to the audio</a> instead.</p>
+                </audio>
+            </li>
+        </ul>
+
+        <ul class="uk-list uk-list-striped">
+            <li>
+                <a href="">Advertisement for Citizens Budget</a>
+            </li>
             <li>
                 <a href="{{ route('2019approved') }}">2019 Approved Budget</a>
             </li>
