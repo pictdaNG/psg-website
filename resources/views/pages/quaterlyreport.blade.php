39a62afae39cbb63858193df8b9b94b34b75f67c
Nanichang <nanipaul68@gmail.com>
2019-05-28 10:56:38 +0100

    added quaterly reports

--- resources/views/pages/quaterlyreport.blade.php
+++ resources/views/pages/quaterlyreport.blade.php
@@ -16,6 +16,34 @@
     <div class="content uk-width-1-2@m">
         <h2 class="bold">Download Quarterly Reports</h2>
 
+        <h4> 2018 Quarterly Reports </h4>
+        <ul class="uk-list uk-list-striped">
+            <li>
+                <a href="{{ URL::asset('assets/documents/2019/QUARTERLY-REPORT-CAPITAL-EXPENDITURE-JANUARY-TO-MARCH-2019-1.pdf') }}">
+                    QUARTERLY REPORT CAPITAL EXPENDITURE JANUARY TO MARCH 2019
+                </a>
+            </li>
+            <li>
+                <a href="{{ URL::asset('assets/documents/2019/QUARTERLY-REPORT-OVERHEAD-COST-JANUARY-TO-MARCH-2019-1.pdf') }}">
+                    QUARTERLY REPORT OVERHEAD COST JANUARY TO MARCH 2019
+                </a>
+            </li>
+            <li>
+                <a href="{{ URL::asset('assets/documents/2019/QUARTERLY-REPORT-PERSONNEL-COST-JANUARY-TO-MARCH-2019-1.pdf') }}">
+                    QUARTERLY REPORT PERSONNEL COST JANUARY TO MARCH 2019
+                </a>
+            </li>
+            <li>
+                <a href="{{ URL::asset('assets/documents/2019/QUARTERLY-REPORT-REVENUE-JANUARY-TO-MARCH-2019-1.pdf') }}">
+                    QUARTERLY REPORT REVENUE JANUARY TO MARCH 2019
+                </a>
+            </li>
+        </ul>
+
+        <hr />
+
+        <h4> 2017 Quarterly Reports </h4>
+
         <ul class="uk-list uk-list-striped">
             <li>
                 <a href="{{ URL::asset('assets/documents/first_qtr_2017.pdf') }}">
@@ -40,6 +68,7 @@
         </ul>
 
         <hr />
+        <h4> 2016 Quarterly Reports </h4>
 
         <ul class="uk-list uk-list-striped">
             <li>
@@ -80,7 +109,7 @@
         </ul>
 
         <hr>
-
+        <h4> 2015 Quarterly Reports </h4>
         <ul class="uk-list uk-list-striped">
             <li>
                 <a href="{{ URL::asset('assets/documents/accountant_general_report_2015.pdf') }}">
