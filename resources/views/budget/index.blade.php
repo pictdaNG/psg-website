673a273b063ef3fe06b1202ada0666a5953e8422
Aef Badmus <realafe21@gmail.com>
2020-05-24 23:31:15 +0100

    modified the feedback button and posts to have correct color

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -19,16 +19,16 @@
 		  <th>
 			<tr>
 				<td>Approved Yearly Budget</td>
-			  <td><a href="{{ route('feedback') }}">Feedback</a></td>
+			  <td><a class="uk-feedback-btn" href="{{ route('feedback') }}">Feedback</a></td>
 			</tr>
 		  </th>
-		  <tbody>				
+		  <tbody>
 				<tr>
 					<td>
 						<a href="https://plug-storage.s3.amazonaws.com/APPROVED%202020%20BUDGET%20V2-02%20lamba-edited.pdf">
 							2020 APPROVED BUDGET
 						</a>
-						
+
 					</td>
 
 					<td></td>
@@ -49,7 +49,7 @@
 					</td>
 					<td></td>
 				</tr>
-				
+
 		  </tbody>
 		</table>
 		<hr />
@@ -71,53 +71,53 @@
 					  </td>
 					</tr>
 				@endforeach
-				
+
 				<tr>
 					<td>
 						<a href="https://plug-storage.s3.eu-west-3.amazonaws.com/PLATEAU+STATE+APPROPRIATION+BILL+2019.pdf">PLATEAU STATE APPROPRIATION BILL 2019</a>
 					</td>
 				</tr>
-				
-				
+
+
 				<tr>
 					<td>
 						<a href="https://plug-storage.s3.eu-west-3.amazonaws.com/plateau_state_approved_budget_2019.pdf">PLATEAU STATE APPROVED BUDGET 2019</a>
 					</td>
 				</tr>
-				
+
 				<tr>
 					<td>
 						<a href="https://plug-storage.s3.eu-west-3.amazonaws.com/MINUTES_AND_REPORT_OF_CITIZENS_BUDGET_TOWN_HALL_MEETING_AUGUST_2019.pdf">MINUTES AND REPORT OF CITIZEN BUDGET TOWN HALL MEETING - AUGUST 2019</a>
 					</td>
 				</tr>
-				
+
 				<tr>
 					<td>
 						<a href="{{ URL::asset('assets/documents/2019/2019-PLATEAU-STATE-APPROVED-BUDGET-1.pdf') }}">2019 PLATEAU STATE APPROVED BUDGET</a>
 					</td>
 				</tr>
-				
+
 				<tr>
 					<td>
 						<a href="https://plug-storage.s3.eu-west-3.amazonaws.com/PLATEAU_CENTRAL_CITIZENS_PARTICIPATION_IN_BUDGET_PROCESS_2019.pdf">CITIZENS PARTICIPATION IN BUDGET PROCESS 2019 PLATEAU CENTRAL</a>
 					</td>
-					
+
 				</tr>
 				<tr>
 					<td>
 						<a href="https://plug-storage.s3.eu-west-3.amazonaws.com/PLATEAU_SOUTH_CITIZENS_PARTICIPATION_IN_BUDGET_PROCESS_2019.pdf">CITIZENS PARTICIPATION IN BUDGET PROCESS 2019 PLATEAU SOUTH</a>
 					</td>
-					
+
 				</tr>
 				<tr>
 					<td>
 						<a href="https://plug-storage.s3.eu-west-3.amazonaws.com/PLATEAU_NORTH_CITIZENS_PARTICIPATION_IN_BUDGET_PROCESS_2019.pdf">CITIZENS PARTICIPATION IN BUDGET PROCESS 2019 PLATEAU NORTH</a>
 					</td>
-					
+
 				</tr>
 				<tr>
 					<td>
-						PLATEAU STATE 2019 CITIZENS ENGAGEMENT IN THE STATE BUDGETING PROCESS RADIO ADVERTISEMENT 
+						PLATEAU STATE 2019 CITIZENS ENGAGEMENT IN THE STATE BUDGETING PROCESS RADIO ADVERTISEMENT
 						<br />
 						<audio controls>
 						  <source src="{{ URL::asset('assets/documents/2019/Advertisement-for-Citizens-Budget.mpeg') }}" type="audio/mpeg">
@@ -125,7 +125,7 @@
 							 a <a href="{{ URL::asset('assets/documents/2019/Advertisement-for-Citizens-Budget.mpeg') }}" download>link to the audio</a> instead.</p>
 						</audio>
 					</td>
-					
+
 				</tr>
 				<tr>
 					<td>
@@ -166,7 +166,7 @@
 		</table>
 		<hr />
         <h4>2018</h4>
-        
+
         <table class="uk-table uk-table-hover uk-table-divider">
 			<!--<thead>-->
 			<!--	<tr>-->
